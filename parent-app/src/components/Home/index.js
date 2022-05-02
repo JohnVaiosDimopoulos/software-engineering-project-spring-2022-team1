1c1270993fa99995299d40d824b94dd24e85094e
Spyrosk <>
2022-05-02 18:49:46 +0300

    added bootstrap-carousel

--- parent-app/src/components/Home/index.js
+++ parent-app/src/components/Home/index.js
@@ -1,48 +1,58 @@
+import { Carousel } from 'react-bootstrap'
 
 export default function Home() {
   return (
     <div class='text-gray-700 p-2'><br/>
-      <div class='bg-cyan rounded-3xl w-11/12 mx-auto p-20'>
-        <h1> Καλωσήρθατε στο e-Parent</h1><br/><br/>
-        <h3>Βρές τώρα τη καλύτερη δραστηριότητα για το παιδί σου από τις αμέτρητες επιλογές</h3><br/>
-        <h5>Δεν ξέρεις από που να αρχίσεις; Δες όλες τις <a href="/" id="a2">Κατηγορίες</a></h5>
+      <div class='bg-cyan rounded-3xl w-11/12 h-96 p-32 mx-auto'>
+        <Carousel>
+          <Carousel.Item>
+            <h1>Καλωσήρθατε στο e-Parent</h1>
+            <p>Το νέο site για γρήγορη εύρεση δραστηριοτήτων στην γειτονιά σου!</p>
+          </Carousel.Item>
+          <Carousel.Item class='text-center'>
+            <h2>Βρείτε τώρα τη καλύτερη δραστηριότητα <br/> για εσάς και το παιδί σας από τις αμέτρητες επιλογές</h2>
+            <p>Δεν ξέρεις από που να αρχίσεις; Δες όλες τις <a href="/">Κατηγορίες</a></p>
+          </Carousel.Item>
+        </Carousel>
       </div>
-      <div class='py-10'>
+      <div class='pt-20'>
         <h2>Κατηγορίες</h2>
       </div>
-      <div class='bg-cyan p-20 place-content-center flex'>
-        <div class='relative bottom-10'>
-          <div class='bg-blue-100 py-4 w-20 h-20 rounded-full'>
+      <div class='bg-cyan p-24 place-content-center flex'>
+        <div class='relative bottom-16'>
+          <div class='bg-blue-100 py-12 w-32 h-32 rounded-full'>
             -image-
           </div>
           Αθλητισμός
         </div>
-        <div class='relative top-10'>
-          <div class='bg-blue-100 py-4 p-2 w-20 h-20 rounded-full'>
+        <div class='relative top-16'>
+          <div class='bg-blue-100 py-12 px-2 w-32 h-32 rounded-full'>
             -image-
           </div>
           Εκδηλώσεις
         </div>
-        <div class='relative bottom-10'>
-          <div class='bg-blue-100 py-4 p-2 w-20 h-20 rounded-full'>
+        <div class='relative bottom-16'>
+          <div class='bg-blue-100 py-12 px-2 w-32 h-32 rounded-full'>
             -image-
           </div>
           Γυμναστήρια
-        </div><div class='relative top-10'>
-          <div class='bg-blue-100 py-4 p-2 w-20 h-20 rounded-full'>
+        </div><div class='relative top-16'>
+          <div class='bg-blue-100 py-12 px-2 w-32 h-32 rounded-full'>
             -image-
           </div>
           Χώροι
         </div>
-        <div class='relative bottom-10'>
-          <div class='bg-blue-100 py-4 p-2 w-20 h-20 rounded-full'>
+        <div class='relative bottom-16'>
+          <div class='bg-blue-100 py-12 px-2 w-32 h-32 rounded-full'>
             -image-
           </div>
           Χόμπι
         </div>
       </div>
-      <div class='py-20'>
+      <div class='pt-20'>
         <h2>Δημοφιλέστερα</h2>
+      </div>
+      <div class='bg-cyan rounded-3xl mx-auto w-11/12 h-80 p-28 place-content-center flex'>
       </div>
     </div>
   );
--- parent-app/src/components/Logobar/index.js
+++ parent-app/src/components/Logobar/index.js
@@ -3,25 +3,27 @@ import { Navbar } from 'react-bootstrap'
 export default function Logobar() {
 
   return (
-    <Navbar class="bg-logobar p-2 h-15 flex">
-      <div class="h-12 p-1 bg-gray-100 rounded-full">
-        <a class="no-underline text-gray-700" href="/" id="a"><h2>Logo</h2></a>
-      </div>
-      <div class='px-4 py-1'>
-        <input class='h-10 px-2 rounded-full' type="text" placeholder="Αναζητήστε κάποια δραστηριότητα.."></input>
-      </div>
-      <div class="text-gray-700 py-2 right-10 absolute flex">
-        <div class='px-1'>
-          <button class='bg-gray-100 h-8 w-20 rounded-full' onclick="window.location.href = '/'">
-            Είσοδος
-          </button>
+    <div class='bg-logobar h-15'>
+      <Navbar class='flex'>
+        <div class='h-12 p-1 bg-gray-100 rounded-full'>
+          <a class='no-underline text-gray-700' href="/" id="a"><h2>Logo</h2></a>
         </div>
-        <div class='px-1'>
-          <button class='bg-gray-100 h-8 w-20 rounded-full' onclick="window.location.href = '/'">
-            Εγγραφή
-          </button>
+        <div class='px-4 py-1'>
+          <input class='h-10 px-2 rounded-full' type="text" placeholder="Αναζητήστε κάποια δραστηριότητα.."></input>
         </div>
-      </div>
-    </Navbar>
+        <div class='text-gray-700 py-2 right-10 absolute flex'>
+          <div class='px-1'>
+            <button class='bg-gray-100 h-8 w-20 rounded-full' onclick="window.location.href = '/'">
+              Είσοδος
+            </button>
+          </div>
+          <div class='px-1'>
+            <button class='bg-gray-100 h-8 w-20 rounded-full' onclick="window.location.href = '/'">
+              Εγγραφή
+            </button>
+          </div>
+        </div>
+      </Navbar>
+    </div>
   );
 }
