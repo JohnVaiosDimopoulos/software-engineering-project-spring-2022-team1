e3e0b900d320f7f54c54776c9580eddf1e99917c
Spyrosk <>
2022-05-03 20:06:08 +0300

    Added card component and cont'd landing page

--- parent-app/src/components/Home/index.js
+++ parent-app/src/components/Home/index.js
@@ -1,4 +1,5 @@
 import { Carousel } from 'react-bootstrap'
+import Card from '../Card/index.js'
 
 export default function Home() {
   return (
@@ -20,39 +21,42 @@ export default function Home() {
       </div>
       <div class='bg-cyan p-24 place-content-center flex'>
         <div class='relative bottom-16'>
-          <div class='bg-blue-100 py-12 w-32 h-32 rounded-full'>
+          <div class='bg-blue-100 py-12 w-32 h-32 rounded-full shadow-lg'>
             -image-
           </div>
-          Αθλητισμός
+          <h5>Αθλητισμός</h5>
         </div>
         <div class='relative top-16'>
-          <div class='bg-blue-100 py-12 px-2 w-32 h-32 rounded-full'>
+          <div class='bg-blue-100 py-12 px-2 w-32 h-32 rounded-full shadow-lg'>
             -image-
           </div>
-          Εκδηλώσεις
+          <h5>Εκδηλώσεις</h5>
         </div>
         <div class='relative bottom-16'>
-          <div class='bg-blue-100 py-12 px-2 w-32 h-32 rounded-full'>
+          <div class='bg-blue-100 py-12 px-2 w-32 h-32 rounded-full shadow-lg'>
             -image-
           </div>
-          Γυμναστήρια
+          <h5>Γυμναστήρια</h5>
         </div><div class='relative top-16'>
-          <div class='bg-blue-100 py-12 px-2 w-32 h-32 rounded-full'>
+          <div class='bg-blue-100 py-12 px-2 w-32 h-32 rounded-full shadow-lg'>
             -image-
           </div>
-          Χώροι
+          <h5>Χώροι</h5>
         </div>
         <div class='relative bottom-16'>
-          <div class='bg-blue-100 py-12 px-2 w-32 h-32 rounded-full'>
+          <div class='bg-blue-100 py-12 px-2 w-32 h-32 rounded-full shadow-lg'>
             -image-
           </div>
-          Χόμπι
+          <h5>Χόμπι</h5>
         </div>
       </div>
       <div class='pt-20'>
         <h2>Δημοφιλέστερα</h2>
       </div>
-      <div class='bg-cyan rounded-3xl mx-auto w-11/12 h-80 p-28 place-content-center flex'>
+      <div class='bg-cyan rounded-3xl mx-auto w-11/12 h-96 p-8 place-content-center flex'>
+        <Card />
+        <Card />
+        <Card />
       </div>
     </div>
   );
--- parent-app/src/components/Logobar/index.js
+++ parent-app/src/components/Logobar/index.js
@@ -6,22 +6,18 @@ export default function Logobar() {
     <div class='bg-logobar h-15'>
       <Navbar class='flex'>
         <div class='h-12 p-1 bg-gray-100 rounded-full'>
-          <a class='no-underline text-gray-700' href="/" id="a"><h2>Logo</h2></a>
+          <a class='no-underline text-gray-700' href='/' id="a"><h2>Logo</h2></a>
         </div>
         <div class='px-4 py-1'>
-          <input class='h-10 px-2 rounded-full' type="text" placeholder="Αναζητήστε κάποια δραστηριότητα.."></input>
+          <input type='text' class='h-10 w-96 px-2 rounded-full' placeholder="Αναζητήστε κάποια δραστηριότητα.."></input>
         </div>
         <div class='text-gray-700 py-2 right-10 absolute flex'>
-          <div class='px-1'>
-            <button class='bg-gray-100 h-8 w-20 rounded-full' onclick="window.location.href = '/'">
-              Είσοδος
-            </button>
-          </div>
-          <div class='px-1'>
-            <button class='bg-gray-100 h-8 w-20 rounded-full' onclick="window.location.href = '/'">
-              Εγγραφή
-            </button>
-          </div>
+          <form class='px-1' action='/'>
+            <input type='button' class='bg-gray-100 h-8 w-20 rounded-full' value='Είσοδος' />
+          </form>
+          <form class='px-1' action='/'>
+            <input type='button' class='bg-gray-100 h-8 w-20 rounded-full' value='Εγγραφή' />
+          </form>
         </div>
       </Navbar>
     </div>
