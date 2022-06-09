bcd3a98240faabd11923891b436480f878d9d7b1
Spyrosk <>
2022-06-09 20:52:06 +0300

    added icons

--- provider-app/src/components/ActionBar.js
+++ provider-app/src/components/ActionBar.js
@@ -1,38 +1,43 @@
+import { FaUserCircle, FaRegBuilding } from 'react-icons/fa';
+import { AiOutlineHome } from 'react-icons/ai';
+import { BiBookAdd, BiStats, BiLogOut } from 'react-icons/bi';
+import { FiActivity } from 'react-icons/fi';
+
 export default function ActionBar() {
   return (
     <div class='bg-cyan text-gray-700 text-center font-light w-44 px-2 mt-2 px-2 py-10 rounded-r-3xl'>
-      <img class='w-36 h-36 ml-2 rounded-full' src={require('../img/logo.svg')} />
+      <FaUserCircle class='w-36 h-36 ml-2 rounded-full'/>
       <div class='mt-2'>Όνομα Παρόχου</div>
       <button class='mt-10 w-full hover:bg-hover pt-2 font-light'>
-        <img class='w-14 h-14 mx-auto' src='' alt='' />
+        <AiOutlineHome class='w-12 h-12 mx-auto'/>
         <div class='w-full border-gray-300 border-b-2 mt-2'>Αρχική</div>
       </button>
       <button class='mt-6 w-full hover:bg-hover pt-2 font-light'>
-        <img class='w-14 h-14 mx-auto' src='' alt='' />
+        <FiActivity class='w-12 h-12 mx-auto'/>
         <div class='border-gray-300 border-b-2 mt-2'>Δραστηριότητες</div>
       </button>
       <button class='mt-6 w-full hover:bg-hover pt-2 font-light'>
-        <img class='w-14 h-14 mx-auto' src='' alt='' />
+        <FaRegBuilding class='w-12 h-12 mx-auto'/>
         <div class='border-gray-300 border-b-2 mt-2'>Υποδομές</div>
       </button>
       <button class='mt-6 w-full hover:bg-hover pt-2 font-light'>
-        <img class='w-14 h-14 mx-auto' src='' alt='' />
+        <BiBookAdd class='w-12 h-12 mx-auto'/>
         <div class='border-gray-300 border-b-2 mt-2'>Νέα Δραστηριότητα</div>
       </button>
       <button class='mt-6 w-full hover:bg-hover pt-2 font-light'>
-        <img class='w-14 h-14 mx-auto' src='' alt='' />
+        <BiBookAdd class='w-12 h-12 mx-auto'/>
         <div class='border-gray-300 border-b-2 mt-2'>Νέα Υποδομή</div>
       </button>
       <button class='mt-6 w-full hover:bg-hover pt-2 font-light'>
-        <img class='w-14 h-14 mx-auto' src='' alt='' />
+        <BiStats class='w-12 h-12 mx-auto'/>
         <div class='border-gray-300 border-b-2 px-10 mt-2'>Στατιστικά</div>
       </button>
       <button class='mt-6 w-full hover:bg-hover pt-2 font-light'>
-        <img class='w-14 h-14 mx-auto' src='' alt='' />
+        <FaUserCircle class='w-12 h-12 mx-auto'/>
         <div class='border-gray-300 border-b-2 px-2 mt-2'>Προφίλ</div>
       </button>
       <button class='mt-6 w-full hover:bg-hover pt-2 font-light'>
-        <img class='w-14 h-14 mx-auto' src='' alt='' />
+        <BiLogOut class='w-12 h-12 mx-auto'/>
         <div class='mt-2'>Αποσύνδεση</div>
       </button>
       </div>
