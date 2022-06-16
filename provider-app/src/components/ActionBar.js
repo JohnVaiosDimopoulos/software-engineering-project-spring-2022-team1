1b43bea3abdd25cb41ab551aea62099e9311b62d
Spyrosk <>
2022-06-16 20:34:15 +0300

    Action-bar buttons now highlight when clicked.

--- provider-app/src/components/ActionBar.js
+++ provider-app/src/components/ActionBar.js
@@ -7,43 +7,52 @@ import { useNavigate } from 'react-router-dom';
 export default function ActionBar() {
   const navigate = useNavigate();
 
+  function go(id, path){
+    navigate(path)
+    for (var obj of document.getElementById('actionBar').getElementsByTagName('button')){
+      obj.style.color = '';
+    }
+    var el = document.getElementById(id);
+    el.style.color = '#93ABB2'
+  }
+
   return (
-    <div class='bg-cyan text-gray-700 text-center font-light w-44 px-2 mt-2 px-2 py-10 rounded-r-3xl shadow-lg'>
-      <button onClick={() => navigate('/profile')} class='w-full'>
+    <div id='actionBar' class='bg-cyan text-gray-700 text-center w-44 px-2 mt-2 px-2 py-10 rounded-r-3xl shadow-lg'>
+      <button onClick={() => go('profile', '/profile')} class='w-full'>
         <FaUserCircle class='w-36 h-36 mx-auto rounded-full'/>
       </button>
       <div class='mt-2'>Όνομα Παρόχου</div>
-      <button onClick={() => navigate('/')} class='mt-10 w-full hover:bg-hover pt-2 font-light'>
+      <button id='home' onClick={() => go('home', '/')} class='mt-10 w-full hover:bg-hover pt-2'>
         <AiOutlineHome class='w-12 h-12 mx-auto'/>
-        <div class='w-full border-gray-300 border-b-2 mt-2'>Αρχική</div>
+        <div class='w-full border-gray-300 border-b-2 mt-2 text-gray-700'>Αρχική</div>
       </button>
-      <button onClick={() => navigate('/activities')} class='mt-6 w-full hover:bg-hover pt-2 font-light'>
+      <button id='activities' onClick={() => go('activities', '/activities')} class='mt-6 w-full hover:bg-hover pt-2'>
         <FiActivity class='w-12 h-12 mx-auto'/>
-        <div class='border-gray-300 border-b-2 mt-2'>Δραστηριότητες</div>
+        <div class='border-gray-300 border-b-2 mt-2 text-gray-700'>Δραστηριότητες</div>
       </button>
-      <button onClick={() => navigate('/facilities')} class='mt-6 w-full hover:bg-hover pt-2 font-light'>
+      <button id='facilities' onClick={() => go('facilities', '/facilities')} class='mt-6 w-full hover:bg-hover pt-2'>
         <FaRegBuilding class='w-12 h-12 mx-auto'/>
-        <div class='border-gray-300 border-b-2 mt-2'>Υποδομές</div>
+        <div class='border-gray-300 border-b-2 mt-2 text-gray-700'>Υποδομές</div>
       </button>
-      <button onClick={() => navigate('/add-activity')} class='mt-6 w-full hover:bg-hover pt-2 font-light'>
+      <button id='add-activity' onClick={() => go('add-activity', '/add-activity')} class='mt-6 w-full hover:bg-hover pt-2'>
         <BiBookAdd class='w-12 h-12 mx-auto'/>
-        <div class='border-gray-300 border-b-2 mt-2'>Νέα Δραστηριότητα</div>
+        <div class='border-gray-300 border-b-2 mt-2 text-gray-700'>Νέα Δραστηριότητα</div>
       </button>
-      <button onClick={() => navigate('/add-facility')} class='mt-6 w-full hover:bg-hover pt-2 font-light'>
+      <button id='add-facility' onClick={() => go('add-facility', '/add-facility')} class='mt-6 w-full hover:bg-hover pt-2'>
         <BiBookAdd class='w-12 h-12 mx-auto'/>
-        <div class='border-gray-300 border-b-2 mt-2'>Νέα Υποδομή</div>
+        <div class='border-gray-300 border-b-2 mt-2 text-gray-700'>Νέα Υποδομή</div>
       </button>
-      <button onClick={() => navigate('/statistics')} class='mt-6 w-full hover:bg-hover pt-2 font-light'>
+      <button id='statistics' onClick={() => go('statistics', '/statistics')} class='mt-6 w-full hover:bg-hover pt-2'>
         <BiStats class='w-12 h-12 mx-auto'/>
-        <div class='border-gray-300 border-b-2 px-10 mt-2'>Στατιστικά</div>
+        <div class='border-gray-300 border-b-2 px-10 mt-2 text-gray-700'>Στατιστικά</div>
       </button>
-      <button onClick={() => navigate('/profile')} class='mt-6 w-full hover:bg-hover pt-2 font-light'>
+      <button id='profile' onClick={() => go('profile', '/profile')} class='mt-6 w-full hover:bg-hover pt-2'>
         <FaUserCircle class='w-12 h-12 mx-auto'/>
-        <div class='border-gray-300 border-b-2 px-2 mt-2'>Προφίλ</div>
+        <div class='border-gray-300 border-b-2 px-2 mt-2 text-gray-700'>Προφίλ</div>
       </button>
-      <button class='mt-6 w-full hover:bg-hover pt-2 font-light'>
+      <button class='mt-6 w-full hover:bg-hover pt-2'>
         <BiLogOut class='w-12 h-12 mx-auto'/>
-        <div class='mt-2'>Αποσύνδεση</div>
+        <div class='mt-2 text-gray-700'>Αποσύνδεση</div>
       </button>
       </div>
   );
